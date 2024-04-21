ecacbfe84eec09f63b43dbff7aa2c7e208f7c7ff
mahmudhasankhan <khanzadeh008@gmail.com>
2024-04-21 17:28:04 +0600

    restructured staging view

--- hr_data_pipeline/models/staging/stg_hr_attendance.sql
+++ hr_data_pipeline/models/staging/stg_hr_attendance.sql
@@ -1,9 +1,9 @@
 SELECT 
 	serialno as seraialnum,
 	employeeid as id,
+	name as employee_name
 	authdatetime as date_time,
 	authdate as date,
 	authtime as time,
 	devicename as location,
-	name as employee_name
 FROM {{ source('hr_source', 'attendance') }}
